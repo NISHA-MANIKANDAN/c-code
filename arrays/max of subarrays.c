dc11c8bfb65b9b8e21cfb9097c604653b5a0e4a4
NISHA-MANIKANDAN <123714599+NISHA-MANIKANDAN@users.noreply.github.com>
2024-05-14 06:28:57 +0530

    Update max of subarrays.c

--- arrays/max of subarrays.c
+++ arrays/max of subarrays.c
@@ -1,3 +1,8 @@
+// Given an array arr[] of size N. 
+// The task is to find the sum of the contiguous subarray within a arr[] with the largest sum. 
+// Example: Input: arr = {-2,-3,4,-1,-2,1,5,-3} Output: 7 Explanation: The subarray {4,-1, -2, 1, 5} has the largest sum 7. 
+// Input: arr = {2} Output: 2 Explanation: The subarray {2} has the largest sum 1.
+    
 #include <stdio.h>
 
 int maxSubarraySum(int arr[], int n) {
